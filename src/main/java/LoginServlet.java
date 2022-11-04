cdba4607b6c9458a3ff9e2af029602ae13123299
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-04 17:16:38 +0530

    Added: Validating Name and Password for the login page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,31 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.regex.Pattern;
+
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = {"/LoginServlet"},
         initParams = {
                 @WebInitParam(name = "user", value = ""),
-                @WebInitParam(name = "password", value = "123")
+                @WebInitParam(name = "password", value = "")
         }
 )
-
 public class LoginServlet extends HttpServlet {
-    String validateName = "[A-Z][a-z]{2,}";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         //get request parameters for userid and Password
         String user = request.getParameter("user");
-        // defining pattern to validate the user name with first letter capital and three minimum characters
-        boolean result = Pattern.compile(validateName).matcher(user).matches();
         String pwd = request.getParameter("pwd");
-        //get servlet configure init params
-        String password = getServletConfig().getInitParameter("password");
         //if result is true and password matches with the input password then login successful
-        if (result && password.equals(pwd)) {
+        if (user.matches("^[A-Z]{1}[a-z]{2,}$") & pwd.matches("^(?=.*[A-z])(?=.*[0-9])[-+_!@#$%^&*.,?0-9A-z]{8,}$")) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
